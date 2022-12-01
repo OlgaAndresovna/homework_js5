12abecaa1aa8b3992af1eff3f32f21014d03a6e5
Olga <olg200919@ya.ru>
2022-12-01 19:07:47 +0300

    Задача 1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*Задача 1*/
+
+const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+}
+console.log(Object.values(numbers).filter(element => element >= 3));
+
