b883a81807245ed9320f6d7908697366e4c68032
Olga <olg200919@ya.ru>
2022-12-01 19:15:01 +0300

    Задача 4

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ console.log(post.comments[1].userId);
 console.log(post.comments[1].text);
 */
 
-/*Задача 3*/
+/*Задача 3
 const products = [
     {
         id: 3,
@@ -62,3 +62,38 @@ const products = [
 
 products.forEach(element => element.price = element.price - element.price * 0.15);
 console.log(products);
+*/
+
+/*Задача 4*/
+
+const products = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ],
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: [],
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg",
+        ],
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+
+const step1filter = products.filter(products => products.photos != undefined);
+console.log(step1filter.filter(step1filter => step1filter.photos.length > 0))
+
+
